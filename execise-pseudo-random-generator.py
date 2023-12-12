cf9405b7287df74348e723ab8a68d36c9643cac4
mila454 <88385276+mila454@users.noreply.github.com>
2023-12-12 15:20:53 +0300

    Update execise-pseudo-random-generator.py

--- execise-pseudo-random-generator.py
+++ execise-pseudo-random-generator.py
@@ -5,9 +5,9 @@ key = b"0"
 entropy = hmac.new(key, msg, hashlib.sha256).digest()
 print("Entropy:", entropy)
 seed = hashlib.sha3_256(entropy).digest()
-print("seed = SHA-256(entropy) =", seed)
+print("seed = ", seed)
 
 for i in range(30):
-    rand = 1 + int.from_bytes(entropy, byteorder = "big") - int.from_bytes(hmac.new(i.to_bytes(8, byteorder = "big", signed=True), seed, hashlib.sha256).digest(), byteorder = "big") % 10
+    rand = 1 + (int.from_bytes(entropy, byteorder = "big") - int.from_bytes(hmac.new(i.to_bytes(8, byteorder = "big", signed=True), seed, hashlib.sha256).digest(), byteorder = "big")) % 10
     print(rand, " ")
     
